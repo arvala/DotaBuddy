445b423a890084002810216fe59c307382e91aa0
Tuomas Jokioja <jokioja92@hotmail.com>
2014-06-14 15:53:47 +0300

    PLS WERK

--- src/DotaBuddy.java
+++ src/DotaBuddy.java
@@ -1,4 +1,27 @@
 
+import java.sql.*;
 public class DotaBuddy {
 
+	private static Connection kanta = null;
+	
+	public static Connection alusta() {
+		try {
+			Class.forName("org.postgresql.Driver").newInstance();
+			kanta = DriverManager.getConnection("jdbc:postgresql:Dota2", "postgres", "admin");
+		}
+		catch (Exception e) {
+			System.out.println("Virhe tietokantakerroksessa: " + e);
+		}
+		return kanta;
+	}
+		
+	public static void sulje() {
+		try {
+			kanta.close();
+		}
+		catch (SQLException e) {
+			System.out.println("Virhe tietokantakerroksessa: " + e);
+		}
+	}
+	
 }
